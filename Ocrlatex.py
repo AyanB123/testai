2f6860b4bbab19128a980c7798755b9882de2ece
Ayan Babwany <babwany@ualberta.ca>
2023-03-02 20:52:11 -0700

    lmao

--- Ocrlatex.py
+++ Ocrlatex.py
@@ -5,7 +5,7 @@ from PIL import Image
 import requests
 
 # Replace the URL with the path to your image
-url = "https://example.com/image.jpg"
+url = "https://d2vlcm61l7u1fs.cloudfront.net/media%2F805%2F8051d2aa-971f-435f-8473-a1b25732fc19%2FphpwH4v8f.png"
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
 
